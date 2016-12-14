df3c99f68feca463bb74e2e6e6eb9c1652111754
askaper <andrew@skaper.info>
2016-12-13 21:44:04 -0600

    Created addToOrder method to set state and push to state when adding fish to order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
     super();
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     // This is the initial state.
     this.state = {
       fishes: {},
@@ -36,6 +37,15 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    //copy state
+    const order = {...this.state.order};
+
+    //push to state
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -43,11 +53,11 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="list-of-fishes">
             {Object.keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
           </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}ß /> {/*this is how we pass props to child components */}
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} /> {/*this is how we pass props to child components */}
       </div>
     )
   }
