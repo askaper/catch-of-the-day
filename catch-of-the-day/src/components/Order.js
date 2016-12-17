75ea40be9b3d500fdec0396da36f6e4733771446
askaper <andrew@skaper.info>
2016-12-17 15:08:13 -0600

    Added PropTypes component validation

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -54,7 +54,12 @@ class Order extends React.Component {
       </div>
     )
   }
-
 }
+
+Order.propTypes = {
+  fishes: React.PropTypes.object.isRequired,
+  order: React.PropTypes.object.isRequired,
+  removeFromOrder: React.PropTypes.func.isRequired
+};
 
 export default Order;
