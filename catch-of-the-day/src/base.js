247f8cd04adc88e3efafd45f13aee621a103799d
askaper <andrew@skaper.info>
2016-12-19 17:09:56 -0600

    Removed Firebase data from public view

--- catch-of-the-day/src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-  apiKey: "AIzaSyDJg4l2ChaGx81E2z-qvOZp3WBrqbZCSbY",
-  authDomain: "catch-of-the-day-8a918.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-8a918.firebaseio.com"
-});
-
-export default base
